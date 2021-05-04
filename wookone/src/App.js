cb7a1a1372c0d85e67cc2f5e9f71db4179840e89
tektechnologies <info@tektechnologies.com>
2021-05-04 10:25:45 -0500

    add project setup

--- wookone/src/App.js
+++ wookone/src/App.js
@@ -54,9 +54,9 @@ function App() {
           <span class="label label-default">News</span>
           <span class="label label-primary">W3Schools</span>
           <span class="label label-success">Labels</span>
-          {/* <span class="label label-info">Football</span>
+          <span class="label label-info">Football</span>
           <span class="label label-warning">Gaming</span>
-          <span class="label label-danger">Friends</span> */}
+          <span class="label label-danger">Friends</span> 
         </p>
       </div>
       <div class="alert alert-success fade in">
