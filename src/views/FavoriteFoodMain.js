598986e024b4cb8a6f56cf46cdcf03e0e6b59ea7
tektechnologies <info@tektechnologies.com>
2021-05-25 15:27:21 -0500

    rendered an image

--- src/views/FavoriteFoodMain.js
+++ src/views/FavoriteFoodMain.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import FavoriteFoods from './FavoriteFoods';
-
+import Taco from '../assets/Images/taco.jpeg';
 class FavoriteFoodMain extends React.Component {
   render() {
     return(
       <FavoriteFoods 
         title="Tacos"
-        src="../assets/images/tacos.jpeg"
+        src={Taco}
         description="A Taco!"
       />
     )
