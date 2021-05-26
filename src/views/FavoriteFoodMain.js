0b309e98d0848a76f89c97c6ac4c1ec4c803116c
tektechnologies <info@tektechnologies.com>
2021-05-26 17:04:08 -0500

    add bootstrap and state and counter to food main

--- src/views/FavoriteFoodMain.js
+++ src/views/FavoriteFoodMain.js
@@ -1,14 +1,47 @@
 import React from 'react';
 import FavoriteFoods from './FavoriteFoods';
-import Taco from '../assets/Images/taco.jpeg';
+import Card from 'react-bootstrap/Card';
+
+
+
 class FavoriteFoodMain extends React.Component {
+
+  constructor(props){
+    this.state = {
+      favorites: 0
+    }
+  }
+
+
+ addFavoriteCount = () => {
+   this.state({favorites: this.state.favorites + 1});
+ }
+
+
+
+
   render() {
     return(
-      <FavoriteFoods 
-        title="Tacos"
-        src={Taco}
-        description="A Taco!"
-      />
+      <Card 
+        style={{width: '18rem'}}
+        bg="dark"
+        text="light"
+        onClick={this.addFavorite}
+        >
+        <Card.Img varitant="top" src={this.props.src} />
+        <Card.Body>
+        <Card.Title>{this.props.title}</Card.Title>
+        <Card.Text>
+        😎 = {this.state.favorites}
+        </Card.Text>
+        <Card.Text> 
+          {this.props.description}
+        </Card.Text>
+
+        </Card.Body>
+        </Card>
+
+    
     )
   }
 }
