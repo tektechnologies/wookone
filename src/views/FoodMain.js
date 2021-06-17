6a14bd82d9d283610936122ff37dbb7bb97da490
tektechnologies <info@tektechnologies.com>
2021-06-17 14:28:37 -0500

    set up food main class for rendering card of FF

---
+++ src/views/FoodMain.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap/CardColumns';
+import CardColumns from 'react-bootstrap/CardColumns';
+import FavoriteFoods from './FavoriteFoods';
+
+class FoodMain extends React.Component {
+
+
+  render(){
+    return(
+
+    )
+  }
+}
+
+export default FoodMain;
