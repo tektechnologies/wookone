980efcda1c753344ec55cc9b5fe7928cce75b153
tektechnologies <info@tektechnologies.com>
2021-06-17 14:47:22 -0500

    add selected food class empty

---
+++ src/views/SelectedFood.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+
+
+class SelectedFood extends React.Component {
+
+
+
+
+}
+
+export default SelectedFood;
