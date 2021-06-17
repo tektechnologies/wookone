74479e9bd446a4e6f4c936103ad602c79efeb2de
tektechnologies <info@tektechnologies.com>
2021-06-17 15:06:43 -0500

    add modal to selected food

--- src/views/SelectedFood.js
+++ src/views/SelectedFood.js
@@ -8,9 +8,35 @@ import Card from 'react-bootstrap/Card';
 
 class SelectedFood extends React.Component {
 
+render(){
+  return (
+    <Modal.Dialog>
+      <Modal.Header closeButton>
+        <Modal.Title>{this.props.selectedFood.title}</Modal.Title>
+      </Modal.Header>
+    
+      <Modal.Body>
+      <Card bg="dark" text="light" onClick={this.displayAsModal}
+      >
+      <Card.Img variant="top" alt={this.props.selectedFood.title} src={this.props.selectedFood.image_url}
+      />
+      
+      <Card.Body>
+        <Card.Title>{this.props.selectedFood.title}</Card.Title>
+        <Card.Text>{this.props.selectedFood.description}</Card.Text>
+      </Card.Body>
+      </Card>
+      </Modal.Body>
+    
+      <Modal.Footer>
+      <Button onClick={this.props.handlClose}
+        variant="secondary">Close</Button>
+      </Modal.Footer>
+    </Modal.Dialog>
 
 
-
+  )
+ }
 }
 
 export default SelectedFood;
