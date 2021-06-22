0e6759bedd1769d9b8795f5cc318e1a32d1316d6
tektechnologies <info@tektechnologies.com>
2021-06-22 14:21:54 -0500

    add options and onChange function call

--- src/views/FoodMain.js
+++ src/views/FoodMain.js
@@ -2,23 +2,32 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CardColumns from 'react-bootstrap/CardColumns';
 import FavoriteFoods from './FavoriteFoods';
-import { Form } from 'react-bootstrap';
+import { Form } from 'react-bootstrap/Form';
+
 
 class FoodMain extends React.Component {
+
+ 
+
+
+
+
 
   render(){
     return(
         <div>
           <Form>
             <Form.Group>
-              <Form.Label></Form.Label>
-              <Form.Control> 
+              <Form.Label>What World?</Form.Label>
+              <Form.Control as="select"
+              onChange={this.filter}> 
+                <option value="">All Paths</option>
                 <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
-                <option value=""></option>
+                <option value="1">Food</option>
+                <option value="2">Trail</option>
+                <option value="3">Nature</option>
+                <option value="4">Technology</option>
+                <option value="5">Ocean</option>
               </Form.Control>
             </Form.Group>
           </Form>
