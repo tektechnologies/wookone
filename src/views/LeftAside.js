0e594c4a477ec732912c9f15d53c667b5c29927b
tektechnologies <info@tektechnologies.com>
2021-06-23 15:33:39 -0500

    add updates to the leftAside

--- src/views/LeftAside.js
+++ src/views/LeftAside.js
@@ -19,19 +19,19 @@ render() {
                         <Card.Text>Interests</Card.Text>
                 </Card.Body>
                 <ListGroup className="list-group-flush">
-                      <ListGroupItem><span className="label label-default">Weather</span></ListGroupItem>
-                      <ListGroupItem><span className="label label-primary">Music</span></ListGroupItem>
-                      <ListGroupItem><span className="label label-success">Farmers Markets</span></ListGroupItem>
-                      <ListGroupItem><span className="label label-info">Hiking</span></ListGroupItem>
-                      <ListGroupItem><span className="label label-primary">Weather</span></ListGroupItem>
-                      <ListGroupItem><span className="label label-success">Friends</span></ListGroupItem>
+                      <ListGroupItem><span className="label label-default"><Card.Link href="/Weather">Weather</Card.Link></span></ListGroupItem>
+                      <ListGroupItem><span className="label label-primary"><Card.Link href="/Weather">Music</Card.Link></span></ListGroupItem>
+                      <ListGroupItem><span className="label label-success"><Card.Link href="/Weather">Farmers Markets</Card.Link></span></ListGroupItem>
+                      <ListGroupItem><span className="label label-info"><Card.Link href="/HikeATrail">Hiking</Card.Link></span></ListGroupItem>
+                      <ListGroupItem><span className="label label-primary"><Card.Link href="/Weather">Weather</Card.Link></span></ListGroupItem>
+                      <ListGroupItem><span className="label label-success"><Card.Link href="/YelpFood">Find Local Food</Card.Link></span></ListGroupItem>
                 </ListGroup>
                 <Card.Body>
-                      <Card.Link href="/HikeATrail">Hike a Trail</Card.Link>
-                      <Card.Link href="/Weather">Weather</Card.Link>
+                    
+                      
                       <Card.Link href="/YourMovies">Your Movies</Card.Link>
                       <Card.Link href="/FavoriteImageMain">Favorite Images</Card.Link>
-                      <Card.Link href="/YelpFood">Find Local Food</Card.Link>
+                      
                 </Card.Body>
             </Card>
           </Col>
