5210fed0d6cfc0bffc809863baf1ed9cc6959ba7
tektechnologies <info@tektechnologies.com>
2021-06-24 15:49:17 -0500

    add weather file template with rce snippet

--- src/views/CityExplorer/Restaurants.js
+++ src/views/CityExplorer/Restaurants.js
@@ -4,15 +4,16 @@ export class Restaurants extends Component {
   render() {
     return (
       <div>
-        <h3>Restaurants in {}</h3>
+        <h3>Restaurants in {this.props.location.search_query}</h3>
         <ul>
-        {
-          
-          <li>
-          <p>Restaruant Name: </p> 
-          <p>Food Genre: </p>
-          
+        { this.props.restaurants && this.props.restaurants.map((place, index) => {
+          <li key={index}>
+            <p>Restaruant Name: {place.restaurant}</p> 
+            <p>Food Genre: {place.restaurant} serves {place.cusine} food in {place.locality} </p>
           </li>
+        })
+          
+        
         
         }
         </ul>
