f8b50e5183f098b0cded2b61b8918bd3844e4d61
tektechnologies <info@tektechnologies.com>
2021-06-24 10:37:15 -0500

    add city explorer files using rcc

---
+++ src/views/CityExplorer/CityExplorerMain.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class CityExplorerMain extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default CityExplorerMain
---
+++ src/views/CityExplorer/Restaurants.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Restaurants extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Restaurants
