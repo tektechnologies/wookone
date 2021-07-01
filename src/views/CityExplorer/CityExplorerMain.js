241c5b0e75e75ace0aac4fb819bab7e24d002774
tektechnologies <info@tektechnologies.com>
2021-07-01 15:49:00 -0500

    add City Main render outline

--- src/views/CityExplorer/CityExplorerMain.js
+++ src/views/CityExplorer/CityExplorerMain.js
@@ -31,7 +31,27 @@ export class CityExplorerMain extends React.Component {
   render() {
     return (
       <div>
-      
+        <Container>
+          <Row>
+            <Col>
+              {/**City search component */}
+            </Col>
+          </Row>
+          {/** Display Map here. */
+            <>
+              <Row>
+                <Col>
+                  {/**Component Lat and Lon  */}
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  {/**Component map */}
+                </Col>
+              </Row>
+            </>
+          }
+        </Container>
       </div>
     )
   }
