91673aa6212cfe507a443e03a2520fb53a085091
tektechnologies <info@tektechnologies.com>
2021-07-01 11:29:12 -0500

    set up file scaffolding

---
+++ src/views/CityExplorer/CityExplorerSearch.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CityExplorerSearch extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
---
+++ src/views/CityExplorer/LatLon.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class LatLon extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
