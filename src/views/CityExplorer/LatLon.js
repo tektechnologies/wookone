615fffc3a6642fa8217304bedf5a3b1afdc38ee0
tektechnologies <info@tektechnologies.com>
2021-07-01 16:00:27 -0500

    add props outline for the latlon component

--- src/views/CityExplorer/LatLon.js
+++ src/views/CityExplorer/LatLon.js
@@ -4,7 +4,8 @@ export default class LatLon extends React.Component {
   render() {
     return (
       <div>
-        
+        <h2>Welcome to {this.props }</h2>
+        <p>{this.props } is located at {this.props } latitude by this { this.props } longitude.</p>
       </div>
     )
   }
