113a4c53ef9822b4bdd45bc343c6eafb4a183632
tektechnologies <info@tektechnologies.com>
2021-07-01 11:57:58 -0500

    empty out renders in city search and main

--- src/views/CityExplorer/CityExplorerMain.js
+++ src/views/CityExplorer/CityExplorerMain.js
@@ -1,10 +1,5 @@
 import React, { Component } from 'react';
 import Map from './Map';
-import Weather from './Weather';
-import Restaurants from './Restaurants';
-import location from '../../assets/data/location.json';
-import restaurants from '../../assets/data/restaurant.json';
-import weather from '../../assets/data/weather.json';
 import map from '../../assets/Images/map.png';
 import axios from 'axios'; 
 import LatLon from './LatLon';
@@ -22,9 +17,7 @@ export class CityExplorerMain extends React.Component {
       longitude: '',
       displaySearchResults: false,
       displayError: false,
-      errorMessage: '',
-      weather: weather,
-      restaurants: restaurants
+      errorMessage: ''
     }
   }
   updateCitySearch = (event) => {
@@ -36,37 +29,7 @@ export class CityExplorerMain extends React.Component {
   render() {
     return (
       <div>
-
-        <div id="header">
-          <h1>Find Your Path</h1>
-          <p>Enter a location in the USA below to learn about the weather, restaurants, movies, and more!</p>
-        </div>
-
-        <form onSubmit={this.handleLocationSearch}>
-          <label>Find the Fun.</label>
-          <input type="text"  name="search" placeholder="Enter a location." />
-          <button type="sumbit">Find It!</button>
-        </form>
-
-        {/**add condtional rendering for search */}
-        { this.state.displaySearchResults &&
-
-          <div>
-            <Map 
-            location={this.state.locationObject}
-            map={map}
-            />
-            <Weather
-            weather={this.state.weather}
-            location={this.state.locationObject}
-            />
-            <Restaurants
-            restaurants={this.state.restaurants}
-            location={this.state.locationObject}
-            />
-         </div>
-
-        }
+      
       </div>
     )
   }
--- src/views/CityExplorer/CityExplorerSearch.js
+++ src/views/CityExplorer/CityExplorerSearch.js
@@ -15,8 +15,16 @@ export default class CityExplorerSearch extends React.Component {
   render() {
     return (
       <div>
-        
-      </div>
+        <div id="header">
+          <h1>Find Your Path</h1>
+          <p>Enter a location in the USA below to learn about the weather, restaurants, movies, and more!</p>
+        </div>
+        <form onSubmit={this.handleLocationSearch}>
+          <label>Find the Fun.</label>
+          <input type="text"  name="search" placeholder="Enter a location." />
+          <button type="sumbit">Find It!</button>
+        </form>
+       </div>
     )
   }
 }
