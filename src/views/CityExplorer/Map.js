f6a4f3cd0094bc937857aa653e5c3b182c9e2527
tektechnologies <info@tektechnologies.com>
2021-07-01 16:14:19 -0500

    add map component props  outline

--- src/views/CityExplorer/Map.js
+++ src/views/CityExplorer/Map.js
@@ -5,10 +5,14 @@ export class Map extends Component {
     
     return (
       <div>
-        <h3>Location Map for { this.props.location.search_query}</h3>
-        { this.props.map && 
-          <img src={this.props.map || ''} alt="temp placeholder" height="250" width="250"/>
-        }
+        <h3>Location Map for { this.props}</h3>
+       
+          <img src={this.props} 
+            alt={} 
+            title={}
+            roundedCircle
+          />
+        
       </div>
     )
   }
