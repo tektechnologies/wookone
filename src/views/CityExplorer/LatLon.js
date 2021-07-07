7a34c1d80bf904e7307d437b846e3b644dbb2481
tektechnologies <info@tektechnologies.com>
2021-07-07 13:43:31 -0500

    add props to lat lon component

--- src/views/CityExplorer/LatLon.js
+++ src/views/CityExplorer/LatLon.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class LatLon extends React.Component {
   render() {
     return (
       <div>
-        <h2>Welcome to {this.props }</h2>
-        <p>{this.props } is located at {this.props } latitude by this { this.props } longitude.</p>
+        <h2>Welcome to {this.props.city }</h2>
+        <p>{this.props.city } is located at {this.props.lat } latitude by this { this.props.lon } longitude.</p>
       </div>
     )
   }
