d0078839379554caaa9af37e9cea18a56915a4c2
tektechnologies <info@tektechnologies.com>
2021-07-14 16:47:06 -0500

    data is rendering from the json

--- src/views/CityExplorer/Weather.js
+++ src/views/CityExplorer/Weather.js
@@ -3,20 +3,19 @@ import React, { Component } from 'react';
 export class Weather extends Component {
   
   render() {
-    console.log('weather page ', this.props.weather.map( weatherObject => weatherObject.date));
-    console.log('location prop', this.props.city);
+    // console.log('weather page ', this.props.weather.map( weatherObject => weatherObject.date));
+    // console.log('weather page ', this.props.weather.map( weatherObject => weatherObject));
+    // console.log('location prop', this.props.city);
     return (
      
-      this.props.weather.map(weatherObject => (
-        <div>
-     
-        
-        
-       
-        weatherObject.date
+      this.props.weather.map((weatherObject, index) => (
+        <div key={index}>
+          <h2>{this.props.city}</h2> 
+          <p>High Temperatures: {weatherObject.max_temp}</p> 
+          <p>Todays Date: {weatherObject.date}</p> 
+          <p>Wind Direction: {weatherObject.wind_cdir}</p> 
+          <p>{weatherObject.description}</p> 
         </div>
-        
-
       ))
       
   
