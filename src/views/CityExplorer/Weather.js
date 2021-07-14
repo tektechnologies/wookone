6710ca917584424c130285eccecb9542f2ddb4c2
tektechnologies <info@tektechnologies.com>
2021-07-14 17:22:49 -0500

    add bootstrap styling needs more

--- src/views/CityExplorer/Weather.js
+++ src/views/CityExplorer/Weather.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Container, Card, Button, Row } from 'react-bootstrap';
+
+
 
 export class Weather extends Component {
   
@@ -10,11 +13,38 @@ export class Weather extends Component {
      
       this.props.weather.map((weatherObject, index) => (
         <div key={index}>
-          <h2>{this.props.city}</h2> 
-          <p>High Temperatures: {weatherObject.max_temp}</p> 
-          <p>Todays Date: {weatherObject.date}</p> 
-          <p>Wind Direction: {weatherObject.wind_cdir}</p> 
-          <p>{weatherObject.description}</p> 
+
+        <Container>
+        <Row>
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="https://picsum.photos/id/281/200/175" />
+          <Card.Body>
+            <Card.Title>{this.props.city}</Card.Title>
+            <Card.Text>
+            High Temperatures: {weatherObject.max_temp}
+            </Card.Text>
+            <Card.Text>
+            Todays Date: {weatherObject.date}
+            </Card.Text>
+            <Card.Text>
+            Wind Direction: {weatherObject.wind_cdir}
+            </Card.Text>
+            <Card.Text>
+            {weatherObject.description}
+            </Card.Text>
+            <Button variant="primary" href="./CityExplorer/CityExplorerMain">
+            Go Here</Button>
+          </Card.Body>
+      </Card>
+      </Row>
+    </Container>    
+        
+
+
+
+
+      
+         
         </div>
       ))
       
